f068e10775e9af9115c4cb1f015872feed1cfb2e
Elliana May <me@mause.me>
2021-10-15 10:55:37 +0800

    Add package_dir

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@
 
 from distutils.core import setup
 
-setup(name='php-parsers', version='1.0', packages=['src'])
+setup(name='php-parsers', version='1.0', packages=['src'], package_dir = {'': 'lib'})
