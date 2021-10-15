9fac9827fa34a48e1d514520bb7b8be7c0fd2156
Elliana May <me@mause.me>
2021-10-15 11:16:30 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+from setuptools import find_packages
 
-setup(name='php-parsers', version='1.0', packages=['src'])
+setup(name='php-parsers', version='1.0', packages=find_packages())
